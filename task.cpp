9b86acf62f17dd76500702ce43659da6ca78a510
Krencz György <krencz88@gmail.com>
2024-09-23 17:10:49 +0200

    Vehicle signal handling + introduced Operational signals

--- task.cpp
+++ task.cpp
@@ -7,17 +7,18 @@ using namespace std;
 enum VehicleType {
     BICYCLE,
     CAR,
-    SCOOTER
+    SCOOTER,
 };
 
 const string VehicleTypeStrings[]={"Bicycle", "Car", "Scooter"};
 
 class Vehicle{
 public:
-    unsigned short count=0;
+    mutable unsigned short count=1;
     VehicleType type;
     string id;
-    Vehicle(VehicleType type, string id):id(id),type(type){}
+    Vehicle(){}
+    Vehicle(VehicleType type, string id):id(id), type(type), count(0){}
     bool operator< (const Vehicle &right) const {
         return id+VehicleTypeStrings[type] < right.id + VehicleTypeStrings[right.type];
     }
@@ -25,57 +26,111 @@ public:
 
 class Bicycle: public Vehicle{
 public:
-    Bicycle(string id):Vehicle(VehicleType::BICYCLE, id){}
+    //Bicycle(string id):Vehicle(VehicleType::BICYCLE, id){}
+    Bicycle(string id){
+        this->id=id;
+        this->type=VehicleType::BICYCLE;
+    }
 };
 
 class Car: public Vehicle{
 public:
-    Car(string id):Vehicle(VehicleType::CAR, id){}
+    //Car(string id):Vehicle(VehicleType::CAR, id){}
+    Car(string id){
+        this->id=id;
+        this->type=VehicleType::CAR;
+    }
 };
 
 class Scooter: public Vehicle{
 public:
-    Scooter(string id):Vehicle(VehicleType::SCOOTER, id){}
+    //Scooter(string id):Vehicle(VehicleType::SCOOTER, id){}
+    Scooter(string id){
+        this->id=id;
+        this->type=VehicleType::SCOOTER;
+    }
 };
 
-set<Vehicle> vehicles;
-
-void Onsignal(Bicycle& b) {
-    Vehicle v(VehicleType::BICYCLE, b.id);
-    vehicles.insert(v);
+enum OperationalSignal {
+    RESET,
+    START,
+    STOP
 }
 
-void Onsignal(Car& c){
-    Vehicle v(VehicleType::CAR, c.id);
-    vehicles.insert(v);
-}
+const Start=OperationalSignal::START;
+const auto Stop=OperationalSignal::STOP;
+const auto Reset=OperationalSignal::RESET;
 
-void Onsignal(Scooter& s){
-    Vehicle v(VehicleType::SCOOTER, s.id);
-    vehicles.insert(v);
-}
+enum States{
+    INIT,
+    ACTIVE,
+    STARTED,
+    STOPPED,
+    ERROR
+};
 
-string GetStatistics() {
-    cout<<vehicles.size()<<endl;
-    string result;
-    for(auto &v: vehicles){
-        result+=v.id + " - " + VehicleTypeStrings[(int)v.type] + " (" + to_string(v.count) + ")\n";
+class MonitoringSystem{
+    set<Vehicle> vehicles;
+    int time_period = 60;
+    string getPlaceholderForCar(VehicleType type){
+        return type == VehicleType::CAR ? "    ":"";
     }
-    return result;
-}
+public:
+    template <class V>
+    void Onsignal(V& vehicle_signal) {
+        Vehicle vehicle(vehicle_signal.type, vehicle_signal.id);
+        auto found = vehicles.find(vehicle);
+        if(found == vehicles.end()) vehicles.insert(vehicle);
+        else found->count++;
+    }
+
+    void Onsignal(OperationalSignal operational_signal) {
+        cout<<"Operational signal recieved:"<< operational_signal<<endl;
+    }
+
+    /*void Onsignal(Bicycle& b) {
+        Vehicle v(b.type, b.id);
+        vehicles.insert(v);
+    }
+
+    void Onsignal(Car& c){
+        Vehicle v(c.type, c.id);
+        vehicles.insert(v);
+    }
+
+    void Onsignal(Scooter& s){
+        Vehicle v(s.type, s.id);
+        vehicles.insert(v);
+    }*/
+
+    string GetStatistics() {
+        string result;
+        for(auto &v: vehicles){
+            result+=v.id + " - " + VehicleTypeStrings[(int)v.type] + getPlaceholderForCar(v.type) + " (" + to_string(v.count) + ")\n";
+        }
+        return result;
+    }
+};
 
 //OnSignal(Reset);
 //OnSignal() -> //signals an error
 
 int main(){
+    MonitoringSystem m;
     Scooter s1("ABC-001");
-    Onsignal(s1);
+    m.Onsignal(s1);
+    m.Onsignal(s1);
     Car c1("ABC-001");
-    Onsignal(c1);
+    m.Onsignal(c1);
     Bicycle b1("ABC-001");
-    Onsignal(b1);
-    Onsignal(b1);
-    cout<<GetStatistics()<<endl;
+    m.Onsignal(b1);
+    Bicycle b2("ABC-002");
+    m.Onsignal(b2);
+    m.Onsignal(s1);
+    cout<<m.GetStatistics()<<endl;
+    m.Onsignal(Reset);
+    m.Onsignal(Start);
+    m.Onsignal(Stop);
 
     return 0;
 }
