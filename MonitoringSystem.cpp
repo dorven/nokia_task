3baf2e22fef48c7c607b6bbe50f2028ec164a870
Krencz György <krencz88@gmail.com>
2024-09-26 12:52:33 +0200

    Huge refactor of the Classes

--- MonitoringSystem.cpp
+++ MonitoringSystem.cpp
@@ -1,137 +1,162 @@
 #include <iostream>
 #include <string>
+#include <set>
 #include <thread>
+#include <mutex>
+#include <atomic> //17
 #include <unistd.h> //17
 
-#include "MonitoringSystem.h"
+#include "Vehicles.cpp"
+#include "Logging.cpp"
 
-// g++ task.cpp -std=c++20 -o MonitoringSystem
+// g++ task.cpp -std=c++20 -o proba /home/krencz/cpp/gtest/googletest/build/lib/libgtest.a
 
 using namespace std;
 
-bool Vehicle::operator< (const Vehicle &right) const {
-    return id+VehicleTypeStrings[type] < right.id + VehicleTypeStrings[right.type];
+Logger logger("logfile.txt");
+
+enum OperationalSignal {
+    RESET,
+    START,
+    STOP
 }
 
-string MonitoringSystem::getPlaceholderForCar(VehicleType type){
-    return type == VehicleType::CAR ? "    ":"";
-}
+const Start=OperationalSignal::START;
+const auto Stop=OperationalSignal::STOP;
+const auto Reset=OperationalSignal::RESET;
 
-string MonitoringSystem::getVehicleLine(const Vehicle& v){
-    return v.id + " - " + VehicleTypeStrings[(int)v.type] + getPlaceholderForCar(v.type) + " (" + to_string(v.count) + ")\n";
-}
+enum States{
+    INIT,
+    ACTIVE,
+    STOPPED,
+    ERROR
+};
 
-MonitoringSystem::MonitoringSystem(){
-    logger.log(Info, "MonitoringSystem started.");
-    resetThread = std::thread(&MonitoringSystem::handlePeriodicReset, this);
-}
-
-MonitoringSystem::~MonitoringSystem(){
-    stopFlag = true;
-    // deatach gives false positive valgrind findings.
-    // use resetThread.join(); if it is a problem. it will make the destructor a bit slower
-    // because it has to wait for the thread to exit gracefully.
-    resetThread.detach();
-    logger.log(Info, "MonitoringSystem stopped.");
-}
-
-void MonitoringSystem::setPeriodicResetInterval(int interval){
-    if(interval<1 || interval>INT32_MAX-1){
-        logger.log(Error, "Invalid periodic reset interval. It should be between 1 and 2147483646.");
-        return;
+class MonitoringSystem{
+    unsigned int PERIODIC_RESET_INTERVAL = 60;
+    std::mutex mtx; // Periodic reset can collide with user operations so we have to lock
+    std::thread resetThread;
+    set<Vehicle> vehicles;
+    States state = INIT;
+    unsigned int errorCounter = 0;
+    std::atomic<bool> stopFlag=false;
+    string getPlaceholderForCar(VehicleType type){
+        return type == VehicleType::CAR ? "    ":"";
     }
-    PERIODIC_RESET_INTERVAL = interval;
-    logger.log(Info, "Periodic reset interval set to " + to_string(PERIODIC_RESET_INTERVAL) + " seconds.");
-}
+    string getVehicleLine(const Vehicle& v){
+        return v.id + " - " + VehicleTypeStrings[(int)v.type] + getPlaceholderForCar(v.type) + " (" + to_string(v.count) + ")\n";
+    }
+public:
+    MonitoringSystem(){
+        logger.log(Info, "MonitoringSystem started.");
+        resetThread = std::thread(&MonitoringSystem::handlePeriodicReset, this);
+    }
+    ~MonitoringSystem(){
+        stopFlag = true;
+        resetThread.detach();
+        logger.log(Info, "MonitoringSystem stopped.");
+    }
+    void setPeriodicResetInterval(int interval){
+        if(interval<1 || interval>INT32_MAX-1){
+            logger.log(Error, "Invalid periodic reset interval. It should be between 1 and 2147483646.");
+            return;
+        }
+        PERIODIC_RESET_INTERVAL = interval;
+        logger.log(Info, "Periodic reset interval set to " + to_string(PERIODIC_RESET_INTERVAL) + " seconds.");
+    }
 
-template <class V>
-void MonitoringSystem::Onsignal(V& vehicleSignal) {
-    mtx.lock();
-    if(state == ACTIVE){
-        auto found = vehicles.find(vehicleSignal);
-        if(found == vehicles.end()){
-            if(vehicles.size() < 1000){
-                vehicles.insert(vehicleSignal);
+    template <class V>
+    void Onsignal(V& vehicleSignal) {
+        mtx.lock();
+        if(state == ACTIVE){
+            auto found = vehicles.find(vehicleSignal);
+            if(found == vehicles.end()){
+                if(vehicles.size() < 1000){
+                    vehicles.insert(vehicleSignal);
+                }
+                else logger.log(
+                    Error,
+                    "Too many vehicles in the system. Cannot add: " + vehicleSignal.id + " - " + VehicleTypeStrings[(int)vehicleSignal.type]
+                );
             }
-            else logger.log(
+            else found->count++;
+        }
+        else if(state == ERROR){
+            errorCounter++;
+            logger.log(
                 Error,
-                "Too many vehicles in the system. Cannot add: " + vehicleSignal.id + " - " + VehicleTypeStrings[(int)vehicleSignal.type]
+                "Error in the camera system. Cannot add: " + vehicleSignal.id + " - " + VehicleTypeStrings[(int)vehicleSignal.type]
             );
         }
-        else found->count++;
+        mtx.unlock();
     }
-    else if(state == ERROR){
-        errorCounter++;
-        logger.log(
-            Error,
-            "Error in the camera system. Cannot add: " + vehicleSignal.id + " - " + VehicleTypeStrings[(int)vehicleSignal.type]
-        );
-    }
-    mtx.unlock();
-}
 
-void MonitoringSystem::Onsignal(OperationalSignal operationalSignal) {
-    switch(operationalSignal){
-        case START:
-            if(state == INIT) state = ACTIVE;
-            break;
-        case STOP:
-            if(state == ACTIVE) state = STOPPED;
-            break;
-        case RESET:
-            mtx.lock();
-            errorCounter = 0;
-            vehicles.clear();
-            state = ACTIVE;
-            mtx.unlock();
-            break;
-        default:
-            cout<<"Error: Invalid operational signal "<<(int)operationalSignal<<endl;
-            break;
+    void Onsignal(OperationalSignal operationalSignal) {
+        switch(operationalSignal){
+            case START:
+                if(state == INIT) state = ACTIVE;
+                break;
+            case STOP:
+                if(state == ACTIVE) state = STOPPED;
+                break;
+            case RESET:
+                mtx.lock();
+                errorCounter = 0;
+                vehicles.clear();
+                state = ACTIVE;
+                mtx.unlock();
+                break;
+            default:
+                cout<<"Error: Invalid operational signal "<<(int)operationalSignal<<endl;
+                break;
+        }
     }
-}
 
-void MonitoringSystem::Onsignal() {
-    state = ERROR;
-    logger.log(Error, "Error in the camera system. MonitoringSystem is in ERROR state.");
-}
-
-string MonitoringSystem::GetStatistics() {
-    mtx.lock();
-    string result;
-    for(auto &v: vehicles){
-        result+=getVehicleLine(v);
+    void Onsignal() {
+        state = ERROR;
+        logger.log(Error, "Error in the camera system. MonitoringSystem is in ERROR state.");
     }
-    mtx.unlock();
-    return result;
-}
-string MonitoringSystem::GetStatistics(VehicleType type) {
-    mtx.lock();
-    string result;
-    for(auto &v: vehicles){
-        if(type == v.type) {
+
+    string GetStatistics() {
+        mtx.lock();
+        string result;
+        for(auto &v: vehicles){
             result+=getVehicleLine(v);
         }
+        mtx.unlock();
+        return result;
     }
-    mtx.unlock();
-    return result;
-}
-unsigned int MonitoringSystem::GetErrorCount() {
-    return errorCounter;
-}
-void MonitoringSystem::handlePeriodicReset()
-{
-    int emplasedSeconds = 0;
-    while(!stopFlag){
-        if(state==STOPPED) emplasedSeconds=0;
-        if(emplasedSeconds >= PERIODIC_RESET_INTERVAL){
-            Onsignal(Reset);
-            emplasedSeconds = 0;
+
+    string GetStatistics(VehicleType type) {
+        mtx.lock();
+        string result;
+        for(auto &v: vehicles){
+            if(type == v.type) {
+                result+=getVehicleLine(v);
+            }
         }
-        sleep(1);
-        emplasedSeconds++;
+        mtx.unlock();
+        return result;
     }
-}
+
+    unsigned int GetErrorCount() {
+        return errorCounter;
+    }
+
+    void handlePeriodicReset()
+    {
+        int emplasedSeconds = 0;
+        while(!stopFlag){
+            if(state==STOPPED) emplasedSeconds=0;
+            if(emplasedSeconds >= PERIODIC_RESET_INTERVAL){
+                Onsignal(Reset);
+                emplasedSeconds = 0;
+            }
+            sleep(1);
+            emplasedSeconds++;
+        }
+    }
+};
 
 /*int main(){
     MonitoringSystem m;
